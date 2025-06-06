0e66ba314cfbb895c999bfa409c8140399a6de19
Greg Ells <gregells@gmail.com>
2025-06-06 13:50:51 -0400

    Add signup functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def home(request):
@@ -7,3 +9,22 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        # Create a 'user' form object using the data from the browser request:
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # Add the new user to the database:
+            user = form.save()
+            # Log the user in to save them from having to login after signup:
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = 'Invalid sign up - try again'
+    # A bad POST or a GET request, so render signup.html with an empty form:
+    form = UserCreationForm()
+    context = { 'form': form, 'error_message': error_message }
+    return render(request, 'registration/signup.html', context)
