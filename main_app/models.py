5ad30dcacd9e8be86b352c8c5321255ec7ce2cfb
Greg Ells <gregells@gmail.com>
2025-06-20 13:41:47 -0400

    Add updated_at field to Expense and Ledger models

--- main_app/models.py
+++ main_app/models.py
@@ -15,6 +15,8 @@ CURRENCIES = (
 
 # Create your models here.
 class Ledger(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=300, blank=True)
     currency = models.CharField(
@@ -22,7 +24,6 @@ class Ledger(models.Model):
         choices=CURRENCIES,
         default=CURRENCIES[0][0]
     )
-    created_at = models.DateTimeField(auto_now_add=True)
     creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ledgers_created')
     members = models.ManyToManyField(User, related_name='ledgers_joined')
 
@@ -35,6 +36,7 @@ class Ledger(models.Model):
 
 class Expense(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateField()
