cdffa375096b7097323ffff6e4eb812cff3cd124
Greg Ells <gregells@gmail.com>
2025-06-25 18:34:23 -0400

    Add profile model’s fields to the user page in the admin portal

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
-from .models import Ledger, Expense
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import Profile, Ledger, Expense
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profile'
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+
 
 # Register your models here.
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Ledger)
 admin.site.register(Expense)
