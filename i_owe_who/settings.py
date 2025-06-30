f47a2744cfcfbf9d1f1edfe147a0907bd86b324b
Greg Ells <gregells@gmail.com>
2025-06-30 15:03:48 -0400

    Set-up .env processing in settings.py

--- i_owe_who/settings.py
+++ i_owe_who/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+import environ
+
+# Initialize environment variables
+environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
