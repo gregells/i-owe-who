f079e13608417ab86acd34ea48507d57c2f2f921
Greg Ells <gregells@gmail.com>
2025-06-30 15:03:58 -0400

    Add photo model and migrations

--- main_app/models.py
+++ main_app/models.py
@@ -80,3 +80,13 @@ class Expense(models.Model):
     
     def get_absolute_url(self):
         return reverse('expenses_detail', kwargs={'expense_id': self.id})
+
+
+class Photo(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    url = models.CharField(max_length=200)
+    expense = models.ForeignKey(Expense, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Photo for {self.expense.name} - @{self.url}"
