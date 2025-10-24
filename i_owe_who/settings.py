1af10441426c09f65e2b353f5135c22bd738d5e5
Greg Ells <gregells@gmail.com>
2025-10-23 22:18:22 -0400

    Rotate local secret key and move to .env file, then parse into settings.py

--- i_owe_who/settings.py
+++ i_owe_who/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 import os
+import secrets
 from pathlib import Path
 
 import environ
@@ -27,7 +28,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ku0*+(gr*zp-#$afb^)y-q!&!(gde2%wia)$dh+^_*qx_@$a5='
+# Get secret key from environment variable or randomly generate one if not found:
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY",
+    default=secrets.token_urlsafe(nbytes=64),
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
